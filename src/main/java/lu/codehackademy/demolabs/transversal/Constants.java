6837341c351778521b77394b9ba04a1fc7dbf26f
Dominique RIGHETTO <dominique.righetto@gmail.com>
2015-02-22 10:45:16 +0100

    Fix SonarQube warning

--- src/main/java/lu/codehackademy/demolabs/transversal/Constants.java
+++ src/main/java/lu/codehackademy/demolabs/transversal/Constants.java
@@ -6,24 +6,33 @@ package lu.codehackademy.demolabs.transversal;
  * @author Dominique Righetto
  * 
  */
-public interface Constants {
+public class Constants {
+
+	/**
+	 * 
+	 * Constructor
+	 *
+	 */
+	private Constants() {
+		// Avoid instanciation
+	}
 
 	/** Logical security role to identity Administrator */
-	String ADMIN_SEC_ROLES = "ROLE_ADMIN";
+	public static final String ADMIN_SEC_ROLES = "ROLE_ADMIN";
 
 	/** Logical security role to identify normal student */
-	String STUDENT_SEC_ROLES = "ROLE_STUDENT";
+	public static final String STUDENT_SEC_ROLES = "ROLE_STUDENT";
 
 	/** JSON content type */
-	String JSON_MIME_TYPE = "application/json";
+	public static final String JSON_MIME_TYPE = "application/json";
 
 	/** FORM content type */
-	String FORM_URLENCODED_MIME_TYPE = "application/x-www-form-urlencoded";
+	public static final String FORM_URLENCODED_MIME_TYPE = "application/x-www-form-urlencoded";
 
 	/** Name of the whitelists property file used for input validation */
-	String INPUT_VALIDATION_WHITELISTS_NAME = "whitelists";
+	public static final String INPUT_VALIDATION_WHITELISTS_NAME = "whitelists";
 
 	/** Numbedr of hash iteration for password */
-	int PASSWORD_HASHING_ROUND = 13;
+	public static final int PASSWORD_HASHING_ROUND = 13;
 
 }
